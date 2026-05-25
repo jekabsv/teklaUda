ec5b63a2aadceb3f858929dcfd133265fdab7577
jekabsv <jekabs.vidrusks@gmail.com>
2026-05-25 11:26:24 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using TeklaUniversalUdaController;
+
+namespace teklaUDA4._8Win
+{
+    public partial class Form1 : Form
+    {
+        private TeklaController _controller = new TeklaController();
+        private Timer _uiRefreshTimer;
+
+        private Label lblStatus;
+        private Label lblSeparator;
+
+        private Panel panelDisconnected;
+        private Label lblInstructions;
+        private Label lblDisconnectedObjectInfo;
+        private Button btnConnect;
+
+        private Panel panelConnected;
+        private Label lblActiveProjectTitle;
+        private Label lblActiveProject;
+        private Label lblSelectedObjectTitle;
+        private TextBox txtSelectedObjectInfo;
+        private Label lblUdaName;
+        private TextBox txtUdaName;
+        private Label lblUdaValue;
+        private TextBox txtUdaValue;
+        private Button btnSetUda;
+
+        public Form1()
+        {
+            InitializeComponentManual();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("Initializing...");
+            _controller.Initialize();
+
+            _uiRefreshTimer = new Timer();
+            _uiRefreshTimer.Interval = 150;
+            _uiRefreshTimer.Tick += OnUiRefreshTick;
+            _uiRefreshTimer.Start();
+        }
+
+        private void OnUiRefreshTick(object sender, EventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                this.BeginInvoke(new EventHandler(OnUiRefreshTick), sender, e);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(_controller.TeklaBinPath))
+            {
+                lblStatus.Text = $"Tekla Version: {_controller.DetectedVersion} | State: Tekla is not running!";
+                lblStatus.ForeColor = Color.FromArgb(255, 102, 102);
+            }
+            else if (_controller.IsTeklaLinked)
+            {
+                lblStatus.Text = $"Tekla Version: {_controller.DetectedVersion} | State: Connected";
+                lblStatus.ForeColor = Color.FromArgb(102, 255, 102);
+            }
+            else
+            {
+                lblStatus.Text = $"Tekla Version: {_controller.DetectedVersion} | State: Ready to Connect";
+                lblStatus.ForeColor = Color.FromArgb(255, 153, 0);
+            }
+
+            string rawObjectInfo = _controller.SelectedObjectInfo ?? "";
+            string formattedObjectInfo = rawObjectInfo;
+
+            if (rawObjectInfo.StartsWith("Type: ") && rawObjectInfo.Contains("GUID:"))
+            {
+                formattedObjectInfo = rawObjectInfo.Replace("GUID:", Environment.NewLine + "GUID: ");
+            }
+
+            if (!_controller.IsTeklaLinked)
+            {
+                panelConnected.Visible = false;
+                panelDisconnected.Visible = true;
+
+                bool processMissing = string.IsNullOrEmpty(_controller.TeklaBinPath);
+                btnConnect.Enabled = !processMissing;
+
+                lblInstructions.Text = "Make sure Tekla Structures is open with a project loaded, then click below to link the application.";
+
+                if (_controller.SelectedObjectInfo != "No object selected.")
+                {
+                    lblDisconnectedObjectInfo.Text = formattedObjectInfo;
+                    lblDisconnectedObjectInfo.Visible = true;
+                }
+                else
+                {
+                    lblDisconnectedObjectInfo.Visible = false;
+                }
+            }
+            else
+            {
+                panelDisconnected.Visible = false;
+                panelConnected.Visible = true;
+
+                lblActiveProject.Text = string.IsNullOrEmpty(_controller.ActiveProject) ? "No active project." : _controller.ActiveProject;
+
+                txtSelectedObjectInfo.Text = formattedObjectInfo;
+
+                bool hasActiveSelection = _controller.CurrentSelectedObject != null;
+                btnSetUda.Enabled = hasActiveSelection;
+            }
+        }
+       
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            _controller.ConnectToTekla();
+        }
+
+        private void btnSetUda_Click(object sender, EventArgs e)
+        {
+            string udaKey = txtUdaName.Text.Trim();
+            string udaVal = txtUdaValue.Text.Trim();
+
+            _controller.CommitUDA(udaKey, udaVal);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _uiRefreshTimer?.Stop();
+
+            if (_controller.EventsInstance != null)
+            {
+                try
+                {
+                    _controller.EventsInstance.GetType().GetMethod("UnRegister").Invoke(_controller.EventsInstance, null);
+                }
+                catch { }
+            }
+        }
+
+        private void InitializeComponentManual()
+        {
+            this.Size = new Size(720, 490);
+            this.Text = "Tekla UDA Controller";
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.BackColor = Color.FromArgb(30, 30, 30);
+            this.Load += Form1_Load;
+            this.FormClosing += Form1_FormClosing;
+
+            Font segoeUiFont = new Font("Segoe UI", 12F, FontStyle.Regular);
+            this.Font = segoeUiFont;
+
+            lblStatus = new Label { Location = new Point(15, 15), Size = new Size(670, 30), ForeColor = Color.White };
+            this.Controls.Add(lblStatus);
+
+            lblSeparator = new Label { Location = new Point(15, 45), Size = new Size(670, 2), BackColor = Color.Gray };
+            this.Controls.Add(lblSeparator);
+
+            panelDisconnected = new Panel { Location = new Point(15, 60), Size = new Size(670, 370), Visible = true };
+
+            lblInstructions = new Label { Location = new Point(10, 10), Size = new Size(650, 50), ForeColor = Color.White };
+            lblDisconnectedObjectInfo = new Label { Location = new Point(10, 65), Size = new Size(650, 50), ForeColor = Color.FromArgb(255, 102, 102), Visible = false };
+
+            btnConnect = new Button { Location = new Point(10, 130), Size = new Size(650, 50), Text = "Connect to Tekla", BackColor = Color.FromArgb(50, 50, 50), ForeColor = Color.White, FlatStyle = FlatStyle.Flat };
+            btnConnect.Click += btnConnect_Click;
+
+            panelDisconnected.Controls.Add(lblInstructions);
+            panelDisconnected.Controls.Add(lblDisconnectedObjectInfo);
+            panelDisconnected.Controls.Add(btnConnect);
+            this.Controls.Add(panelDisconnected);
+
+
+            panelConnected = new Panel { Location = new Point(15, 60), Size = new Size(670, 370), Visible = false };
+
+            lblActiveProjectTitle = new Label { Location = new Point(10, 0), Size = new Size(650, 25), Text = "Active Project:", ForeColor = Color.DarkGray };
+            lblActiveProject = new Label { Location = new Point(10, 25), Size = new Size(650, 45), ForeColor = Color.White };
+
+            lblSelectedObjectTitle = new Label { Location = new Point(10, 75), Size = new Size(650, 25), Text = "Selected Tekla Object:", ForeColor = Color.DarkGray };
+            txtSelectedObjectInfo = new TextBox { Location = new Point(10, 100), Size = new Size(650, 60), Multiline = true, ReadOnly = true, BackColor = Color.FromArgb(45, 45, 45), ForeColor = Color.White, BorderStyle = BorderStyle.FixedSingle };
+
+            lblUdaName = new Label { Location = new Point(10, 175), Size = new Size(650, 25), Text = "New UDA Name:", ForeColor = Color.White };
+            txtUdaName = new TextBox { Location = new Point(10, 200), Size = new Size(650, 30), BackColor = Color.FromArgb(45, 45, 45), ForeColor = Color.White, BorderStyle = BorderStyle.FixedSingle, MaxLength = 127 };
+
+            lblUdaValue = new Label { Location = new Point(10, 240), Size = new Size(650, 25), Text = "UDA Value:", ForeColor = Color.White };
+            txtUdaValue = new TextBox { Location = new Point(10, 265), Size = new Size(650, 30), BackColor = Color.FromArgb(45, 45, 45), ForeColor = Color.White, BorderStyle = BorderStyle.FixedSingle, MaxLength = 127 };
+
+            btnSetUda = new Button { Location = new Point(10, 315), Size = new Size(650, 50), Text = "Set UDA to Selected Object", BackColor = Color.FromArgb(50, 50, 50), ForeColor = Color.White, FlatStyle = FlatStyle.Flat };
+            btnSetUda.Click += btnSetUda_Click;
+
+            panelConnected.Controls.Add(lblActiveProjectTitle);
+            panelConnected.Controls.Add(lblActiveProject);
+            panelConnected.Controls.Add(lblSelectedObjectTitle);
+            panelConnected.Controls.Add(txtSelectedObjectInfo);
+            panelConnected.Controls.Add(lblUdaName);
+            panelConnected.Controls.Add(txtUdaName);
+            panelConnected.Controls.Add(lblUdaValue);
+            panelConnected.Controls.Add(txtUdaValue);
+            panelConnected.Controls.Add(btnSetUda);
+            this.Controls.Add(panelConnected);
+        }
+    }
+}
---
+++ TeklaController.cs
@@ -0,0 +1,391 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Collections.Generic;
+
+namespace TeklaUniversalUdaController
+{
+    public class TeklaController
+    {
+        public string TeklaBinPath = string.Empty;
+        public bool IsTeklaLinked = false;
+        public string DetectedVersion = "None";
+
+        public Assembly ModelAssembly;
+        public object ModelInstance;
+        public object EventsInstance;
+
+        public string ActiveProject = string.Empty;
+        public string SelectedObjectInfo = "No object selected.";
+        public object CurrentSelectedObject = null;
+
+        private readonly HashSet<string> _validatedVersions = new() { "2022.0", "2023.0", "2024.0", "2025.0", "2026.0" };
+        private readonly object _selectionLock = new object();
+
+        private static readonly HashSet<string> _systemPassthrough = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+        {
+            "System.Memory",
+            "System.Buffers",
+            "System.Runtime.CompilerServices.Unsafe",
+            "System.Numerics.Vectors",
+            "System.Threading.Tasks.Extensions",
+        };
+
+        public void Initialize()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += OnResolveTeklaAssemblies;
+            DetectTekla();
+        }
+
+        public void DetectTekla()
+        {
+            Process[] processes = Process.GetProcessesByName("TeklaStructures");
+            if (processes.Length > 0)
+            {
+                try
+                {
+                    string exePath = processes[0].MainModule.FileName;
+                    TeklaBinPath = Path.GetDirectoryName(exePath);
+
+                    FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(exePath);
+                    DetectedVersion = $"{fileVersionInfo.ProductMajorPart}.{fileVersionInfo.ProductMinorPart}";
+
+                    Console.WriteLine($"Location Path: {TeklaBinPath}");
+                    Console.WriteLine($"Active Version: {DetectedVersion}");
+
+                    if (!_validatedVersions.Contains(DetectedVersion))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("WARNING! Tekla version falls outside validated database... ");
+                        Console.ResetColor();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"ERROR! Could not extract execution data trace: {ex.Message}");
+                }
+                finally
+                {
+                    foreach (var p in processes) p?.Dispose();
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("WARNING! TeklaStructures process was not found running on this system.");
+                Console.ResetColor();
+            }
+        }
+
+        public Assembly OnResolveTeklaAssemblies(object sender, ResolveEventArgs args)
+        {
+            string shortName = new AssemblyName(args.Name).Name;
+
+            if (!_systemPassthrough.Contains(shortName))
+            {
+                if (shortName.StartsWith("System.", StringComparison.OrdinalIgnoreCase) || shortName.StartsWith("Microsoft.", StringComparison.OrdinalIgnoreCase)
+                    || shortName.Equals("mscorlib", StringComparison.OrdinalIgnoreCase)
+                    || shortName.Equals("netstandard", StringComparison.OrdinalIgnoreCase)
+                    || shortName.Equals("WindowsBase", StringComparison.OrdinalIgnoreCase)
+                    || shortName.Equals("PresentationCore", StringComparison.OrdinalIgnoreCase)
+                    || shortName.Equals("PresentationFramework", StringComparison.OrdinalIgnoreCase)
+                    || shortName.EndsWith(".resources", StringComparison.OrdinalIgnoreCase))
+                    return null;
+            }
+
+            string dllName = $"{shortName}.dll";
+
+            bool requiresVersionMatch =
+                shortName.Equals("Tekla.Structures", StringComparison.OrdinalIgnoreCase) ||
+                shortName.StartsWith("Tekla.Structures.", StringComparison.OrdinalIgnoreCase);
+
+            string expectedMajor = DetectedVersion.Split('.')[0];
+
+            List<string> directPaths = new List<string>
+            {
+                Path.Combine(TeklaBinPath, dllName),
+                Path.Combine(TeklaBinPath, "NetCoreRuntime", dllName),
+                Path.Combine(TeklaBinPath, "nt", "bin", dllName),
+                Path.Combine(TeklaBinPath, "plugins", dllName)
+            };
+
+            string rootDrive = Path.GetPathRoot(TeklaBinPath);
+            string environmentCommon = Path.Combine(rootDrive, "TeklaStructures", DetectedVersion, "environments", "common", "extensions");
+            directPaths.Add(Path.Combine(environmentCommon, dllName));
+
+            foreach (string targetDllPath in directPaths)
+            {
+                if (targetDllPath.IndexOf("Net48Runtime", StringComparison.OrdinalIgnoreCase) >= 0) continue;
+                if (!File.Exists(targetDllPath)) continue;
+
+                if (requiresVersionMatch)
+                {
+                    string ver = GetFileVersion(targetDllPath);
+                    if (ver == null || !ver.StartsWith(expectedMajor + "."))
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine($"skipping version-mismatched copy ({ver ?? "unknown"}) at {targetDllPath}");
+                        Console.ResetColor();
+                        continue;
+                    }
+                }
+
+                return LogAndLoad(targetDllPath);
+            }
+
+            string parentDir = Directory.GetParent(TeklaBinPath).FullName;
+            if (Directory.Exists(parentDir))
+            {
+                //Console.WriteLine($"WARNING! Deep scanning installation tree under {parentDir}...");
+                var foundFiles = Directory.GetFiles(parentDir, dllName, SearchOption.AllDirectories);
+
+                var ranked = foundFiles
+                    .Select(f => new
+                    {
+                        Path = f,
+                        Version = GetFileVersion(f),
+                        IsNet48 = f.IndexOf("Net48Runtime", StringComparison.OrdinalIgnoreCase) >= 0,
+                        IsSubApp = f.IndexOf(@"\applications\", StringComparison.OrdinalIgnoreCase) >= 0,
+                    })
+                    .Where(c => !requiresVersionMatch ||
+                                (c.Version != null && c.Version.StartsWith(expectedMajor + ".")))
+                    .OrderBy(c => c.IsSubApp ? 1 : 0)
+                    .ThenBy(c => c.IsNet48 ? 1 : 0)
+                    .ToList();
+
+                if (ranked.Count > 0)
+                {
+                    return LogAndLoad(ranked[0].Path);
+                }
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(
+                    $"No copy of {dllName} on disk matches running Tekla {DetectedVersion}. " +
+                    $"Candidates examined: {foundFiles.Length}.");
+                Console.ResetColor();
+            }
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"UNRESOLVED {dllName}");
+            Console.ResetColor();
+            return null;
+        }
+
+        private string GetFileVersion(string path)
+        {
+            try
+            {
+                return FileVersionInfo.GetVersionInfo(path).FileVersion;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private Assembly LogAndLoad(string path)
+        {
+            string ver = GetFileVersion(path) ?? "unknown";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Success -> Loading v{ver} from: {path}");
+            Console.ResetColor();
+            return Assembly.LoadFrom(path);
+        }
+
+        public void ConnectToTekla()
+        {
+            if (IsTeklaLinked)
+            {
+                Console.WriteLine("Aborted: Connection is already active.");
+                return;
+            }
+            if (string.IsNullOrEmpty(TeklaBinPath))
+            {
+                Console.WriteLine("Aborted: Valid Tekla binary directory is missing.");
+                return;
+            }
+
+            try
+            {
+                Assembly.Load("Tekla.Structures");
+
+                Console.WriteLine("Loading Tekla.Structures.Model assembly...");
+                ModelAssembly = Assembly.Load("Tekla.Structures.Model");
+
+                Type modelType = ModelAssembly.GetType("Tekla.Structures.Model.Model");
+                if (modelType == null)
+                {
+                    throw new TypeLoadException("Could not extract Model reference structure from assemblies.");
+                }
+
+                Console.WriteLine("Instantiating Model instance...");
+                ModelInstance = Activator.CreateInstance(modelType);
+
+                MethodInfo connectionMethod = modelType.GetMethod("GetConnectionStatus");
+                if (connectionMethod == null)
+                {
+                    throw new MissingMethodException("Method GetConnectionStatus not found on Model type.");
+                }
+
+                bool isConnected = (bool)connectionMethod.Invoke(ModelInstance, null);
+
+                if (isConnected)
+                {
+                    IsTeklaLinked = true;
+                    ResolveActiveProject(modelType);
+                    RegisterSelectionEvents();
+                }
+                else
+                {
+                    SelectedObjectInfo = "Tekla detected, but no open model was found.";
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Connection established with process, but no active model is open in Tekla Structures.");
+                    Console.ResetColor();
+                }
+            }
+            catch (Exception ex)
+            {
+                Exception trueException = (ex is TargetInvocationException && ex.InnerException != null) ? ex.InnerException : ex;
+
+                SelectedObjectInfo = $"Link Exception: {trueException.Message}";
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"\nERROR!: {trueException.Message}");
+                Console.WriteLine($"Source: {trueException.Source}");
+                Console.WriteLine($"Stack Trace:\n{trueException.StackTrace}\n");
+                Console.ResetColor();
+            }
+        }
+
+        public void ResolveActiveProject(Type modelType)
+        {
+            try
+            {
+                object modelInfo = modelType.GetMethod("GetInfo").Invoke(ModelInstance, null);
+                Type infoType = modelInfo.GetType();
+
+                string name = infoType.GetProperty("ModelName").GetValue(modelInfo) as string;
+                string path = infoType.GetProperty("ModelPath").GetValue(modelInfo) as string;
+
+                ActiveProject = $"{name} [{path}]";
+                Console.WriteLine($"Connected to workspace: {name}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"WARNING! Could not resolve project paths: {ex.Message}");
+                ActiveProject = "Active project could not be tracked.";
+            }
+        }
+
+        public void RegisterSelectionEvents()
+        {
+            try
+            {
+                Type eventsType = ModelAssembly.GetType("Tekla.Structures.Model.Events");
+                if (eventsType == null)
+                    throw new TypeLoadException("Tekla.Structures.Model.Events not found in Model assembly.");
+
+                EventsInstance = Activator.CreateInstance(eventsType);
+
+                EventInfo selectionEvent = eventsType.GetEvent("SelectionChange");
+                if (selectionEvent == null)
+                {
+                    Console.WriteLine("Events on Events type:");
+                    foreach (var e in eventsType.GetEvents())
+                        Console.WriteLine($" - {e.Name} ({e.EventHandlerType?.Name})");
+                    throw new MissingMemberException("SelectionChange not found.");
+                }
+
+                MethodInfo handlerMethod = typeof(TeklaController).GetMethod(
+                    nameof(OnTeklaSelectionChangedCallback),
+                    BindingFlags.NonPublic | BindingFlags.Instance);
+
+                Delegate del = Delegate.CreateDelegate(selectionEvent.EventHandlerType, this, handlerMethod);
+                selectionEvent.AddMethod.Invoke(EventsInstance, new object[] { del });
+
+                eventsType.GetMethod("Register").Invoke(EventsInstance, null);
+            }
+            catch (Exception ex)
+            {
+                Exception real = (ex is TargetInvocationException && ex.InnerException != null) ? ex.InnerException : ex;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"ERROR! {real.GetType().Name}: {real.Message}");
+                Console.ResetColor();
+            }
+        }
+
+        private void OnTeklaSelectionChangedCallback()
+        {
+            lock (_selectionLock)
+            {
+                try
+                {
+                    Type selectorType = ModelAssembly.GetType("Tekla.Structures.Model.UI.ModelObjectSelector");
+                    if (selectorType == null)
+                    {
+                        SelectedObjectInfo = "ModelObjectSelector type not found.";
+                        return;
+                    }
+
+                    object selector = Activator.CreateInstance(selectorType);
+                    object enumerator = selectorType.GetMethod("GetSelectedObjects").Invoke(selector, null);
+                    Type enumType = enumerator.GetType();
+
+                    bool hasNext = (bool)enumType.GetMethod("MoveNext").Invoke(enumerator, null);
+                    if (hasNext)
+                    {
+                        CurrentSelectedObject = enumType.GetProperty("Current").GetValue(enumerator);
+                        Type objType = CurrentSelectedObject.GetType();
+
+                        object identifier = objType.GetProperty("Identifier").GetValue(CurrentSelectedObject);
+                        Guid guid = (Guid)identifier.GetType().GetProperty("GUID").GetValue(identifier);
+
+                        SelectedObjectInfo = $"Type: {objType.Name}\nGUID: {guid}";
+                        Console.WriteLine($" {objType.Name} / {guid}");
+                    }
+                    else
+                    {
+                        CurrentSelectedObject = null;
+                        SelectedObjectInfo = "No object selected inside workspace.";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Exception real = (ex is TargetInvocationException && ex.InnerException != null) ? ex.InnerException : ex;
+                    SelectedObjectInfo = $"Error mapping object properties: {real.Message}";
+                    Console.WriteLine($"ERROR! {real.Message}");
+                }
+            }
+        }
+
+        public void CommitUDA(string udaKey, string udaVal)
+        {
+            if (CurrentSelectedObject == null) return;
+
+            try
+            {
+                Type objType = CurrentSelectedObject.GetType();
+                MethodInfo setPropertyMethod = objType.GetMethod("SetUserProperty", new[] { typeof(string), typeof(string) });
+
+                bool success = (bool)setPropertyMethod.Invoke(CurrentSelectedObject, new object[] { udaKey, udaVal });
+
+                if (success)
+                {
+                    objType.GetMethod("Modify").Invoke(CurrentSelectedObject, null);
+
+                    Type modelType = ModelInstance.GetType();
+                    modelType.GetMethod("CommitChanges", new[] { typeof(string) })
+                              .Invoke(ModelInstance, new object[] { $"Modified UDA: {udaKey}" });
+
+                    Console.WriteLine($"Pushed UDA: {udaKey} = {udaVal}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR! failed: {ex.Message}");
+            }
+        }
+    }
+}
